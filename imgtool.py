6c78015a80f056e04750a565d8b7e8846f4bd3f3
Nicole Stevens <nicci@ducksfeet.com>
2018-05-09 12:51:14 -0700

    Added formatting with extended tags from EXIF data. Removed fileExif.cameraNameForFile and replaced with simpler method fileExif.exifNameForFile which uses the extended formatting changed default format to @Exif.Image.Model[1]_%Y%m%d%H%M%S@File.ext

--- imgtool.py
+++ imgtool.py
@@ -6,8 +6,7 @@ from argparse import ArgumentParser
 toolversion = '0.1'
 
 description = """
-Imgtool version {} - Copyright 2018 Nicole Stevens - Image manipulation tool
-\n
+Imgtool version {} - Copyright 2018 Nicole Stevens - Image manipulation tool:  
 Rename, reorient, resize and or timestamp jpg files with a unique name based on
 camera model and taken date stored in the exif header. By default imgtool updates
 the atime/mtime timestamps on a file to match the exif header data. 
@@ -32,14 +31,38 @@ limitations under the License.
 """.format(description)
 
 geometry_help = """
-    GEOMETRY is used with the -z or --resize options are used.
+GEOMETRY is used with the -z or --resize options are used.
 
-    Geometry can be specified as a percentage of the overall image or as a pair of width:height. 
-    Width and height are specified in pixels. If width is specified but no height, e.g., 1000:
-    the image will be reized to a width of 1000px with a height calculated in relation to width to
-    maintain the image aspect ratio. Conversely, height is specified without a width, e.g., :1000
-    will resize the image to 1000px high with a width calculated to maintain the aspect ratio. 
-    When the width and height are spcecified no attempt to maintain the aspect ratio is made.
+Geometry can be specified as a percentage of the overall image or as a pair of [width]x[height]. 
+Width and height are specified in pixels. If width is specified but no height, e.g., 1000x
+the image will be reized to a width of 1000px with a height calculated in relation to width to
+maintain the image aspect ratio. Conversely, height is specified without a width, e.g., x1000
+will resize the image to 1000px high with a width calculated to maintain the aspect ratio. 
+When the width and height are spcecified no attempt to maintain the aspect ratio is made.
+"""
+
+format_help = """
+Formatting filenames for automatic renaming is as follows:
+    [@|+]type.Tag%timefmt
+
+Tags starting with @ have their spaces replaced with periods (.), Tags starting with + 
+do not have spaces replaced. There are two types of tags, File and Exif. File Tags are:
+    File.Name:  Filename of the image without extension
+    File.Ext:   Extension of filename, e.g., .JPG 
+    File.ext:   Extension of filename converted to lower-case, e.g., .jpg
+    File.Fullname: Full name of file with directories.
+    File.mtime: String representation YYYYMMDDhhmmss of the file's timestamp in the filesystem.
+
+ExifTags vary by image and camera. The program exiftool may be useful in finding appropriate tags. 
+With ExifTags, if preceeded by brackets with a number, a substring of the tag value, split by spaces will 
+be returned. These substrings start with 0. If @Exif.Image.Model returns NIKON D3400 then the format string @Exif.Image.Model[1] will return 
+D3400. 
+
+Any EXIF Tag present in the image EXIF header can be used to create all or part of a file name. For example, 
+@Image.Make[1]_@Image.
+
+Time formatting, using the EIXF header's image time, is formatted with strftime formatting. See strftime(3).
+
 """
 
 progwarning = """
@@ -47,7 +70,7 @@ WARNING: This tool was written to work with my photographs. It may even be destr
 data is always a good idea before employing automatic tools that can recurse directories. If you 
 break something, you own the remaining pieces."""
 
-defaultTimeFormat='%Y%m%d%H%M%S'
+defaultTimeFormat='@Exif.Image.Model[1]_%Y%m%d%H%M%S@File.ext'
 timeformatHelp='Set the time format used for naming files, in python strftime format, default is "{}"'.format(defaultTimeFormat.replace('%','%%'))
 
 recurse = False
@@ -56,7 +79,10 @@ dry = True
 
 class fileExif:
     """ Read file exif data and process tags """
-    def __init__(self,file,timeformat = '%Y%m%d%H%M%S'):
+    def __init__(self,file,timeformat = defaultTimeFormat):
+        self._renospc = r'\@[A-Za-z\.]*(\[[0-9]*\])?'
+        self._renoxlt = r'\+[A-Za-z\.]*(\[[0-9]*\])?'
+
         self.timeformat = timeformat
         try:
             self.mtime = os.stat(file).st_mtime
@@ -64,16 +90,31 @@ class fileExif:
             print >> sys.stderr, "Cannot stat {}: {}".format(file,e)
             
         self.file = file
-        with open(file,'rb') as f:
-            e = exifread.process_file(f,details=False)
+        try:
+            e = pyexiv2.ImageMetadata(self.file)
+            e.read()
+        except:
+            e = None
         self.exif = e
+        self.fileInfo = {'File.Name': os.path.splitext(os.path.basename(self.file))[0],
+                         'File.Ext': os.path.splitext(os.path.basename(self.file))[1],
+                         'File.ext': os.path.splitext(os.path.basename(self.file))[1].lower(),
+                         'File.FullName': self.file,
+                         'File.mtime': time.strftime('%Y%m%d%H%M%S',time.localtime(self.mtime))
+                    }
 
+    def fileTag(self,tag):
+        if tag in self.fileInfo:
+            return self.fileInfo[tag]
+        return ''
     def adjustOrientation(self):
         degrees = {3: 180, 6: 270, 8: 90}
+        if self.exif is None:
+            if verbose:
+                print 'Not rotating image {}: No EXIF data.'.format(self.file)
+            return
         try:
-            m = pyexiv2.ImageMetadata(self.file)
-            m.read()
-            if m['Exif.Image.Orientation'].value in degrees:
+            if self.exif['Exif.Image.Orientation'].value in degrees:
                 image = Image.open(self.file)
                 rot = degrees[m['Exif.Image.Orientation'].value]
                 if verbose:
@@ -81,14 +122,13 @@ class fileExif:
                 if not dry:
                     image=image.rotate(rot, expand=True)
                     image.save(self.file)
-                    m['Exif.Image.Orientation'].value = 1
-                    m.write()
+                    self.exif['Exif.Image.Orientation'].value = 1
+                    self.exif.write()
             else:
                 if verbose:
                     print 'Not rotating image, {}, either {} is not in list or does not need rotation.'.format(self.file,m['Exif.Image.Orientation'].value)
         except Exception as e:
-            m = 'Cannot rotate image {}: {}'.format(self.file,e)
-            raise IOError(m)
+            raise IOError('Cannot rotate image {}: {}'.format(self.file,e))
 
     def __coord(self,s):
         if s == '':
@@ -105,7 +145,7 @@ class fileExif:
                 p = float(geometry.split('%')[0])/100.0
                 [rw,rh] = map(lambda x: x*p, [imw,imh])
             else:
-                [rw,rh] = map(self.__coord,geometry.split(':'))
+                [rw,rh] = map(self.__coord,geometry.split('x'))
                 if rh and not rw:
                     hpercent = rh/imh
                     rw = imw*hpercent
@@ -121,16 +161,45 @@ class fileExif:
             if not dry:
                 i.save(self.file, exif=imexif)
         except Exception as e:
-            m='Cannot resize {}: {}'.format(self.file,e)
-            raise IOError(m)
+            raise IOError('Cannot resize {}: {}'.format(self.file,e))
 
 
     def tag(self,tag,_type = str):
+        tag = 'Exif.{}'.format(tag.replace(' ','.'))
         try:
             tdata = self.exif[tag]
         except KeyError:
             return None
         return _type(tdata)
+
+    def _extractExif2(self,match):
+        tag = '{}'.format(str(match.group())[1:])
+        if tag.startswith('File.'):
+            return self.fileTag(tag)
+        try:
+            p = match.groups()[0]
+            if p:
+                tag = tag.replace(p,'')
+                p = re.sub(r'[\[\]]','',p)
+                if len(p):
+                    p = int(p)
+        except:
+            p = None
+        try:
+            x = str(self.exif[tag].value)
+        except:
+            x = ''
+        if type(p) is int:
+            xa = x.split(' ')
+            if p < len(xa):
+                x = xa[p]
+        return x
+
+    def _extractExif1(self,match):
+        return self._extractExif2(match).replace(' ','.')
+
+    def formatStringExif(self,str):
+        return time.strftime(re.sub(self._renoxlt,self._extractExif2,re.sub(self._renospc,self._extractExif1, str)),time.localtime(self.fileDate()))
 
     def fileDate(self):
         """ get a EPOCH time value from exif data """
@@ -141,29 +210,15 @@ class fileExif:
         except:
             return self.mtime
 
-    def cameraNameForFile(self):
+    def exifNameForFile(self):
         """ get a new file name for loaded image based on the camera make and model. 
             of those cannot be determined return the original name. """
         ext = os.path.splitext(self.file)[1]
         path =  os.path.dirname(self.file)
-        epoch = self.fileDate()
-        timestr = time.strftime(self.timeformat,time.localtime(epoch))
+        file = self.formatStringExif(self.timeformat)
+        file = re.sub(r'^_','',file)
 
-        try:
-            mk = self.tag('Image Make')
-            md = self.tag('Image Model').split(' ')[1]
-        except:
-            if verbose:
-                print >> sys.stderr, '{}: Cannot get Image.Make or Image.Model'.format(self.file)
-            if epoch and epoch > 0:   # Only rename if there is an emedded date
-                return '{}{}'.format(timestr,ext),epoch
-            return self.file,self.mtime
-
-
-        if mk is not '' and md is not '':
-            n = os.path.join(path,'{}_{}{}'.format(md,timestr,ext.lower()))
-            return n,epoch
-        return src
+        return os.path.join(path,file),self.fileDate()
 
 def setFileInfo(path,fname,exif,rename):
     info = ''
@@ -171,7 +226,7 @@ def setFileInfo(path,fname,exif,rename):
     tstamp = exif.fileDate()
     newname = False
     if rename:
-        newname,tstamp = exif.cameraNameForFile()
+        newname,tstamp = exif.exifNameForFile()
         if not newname == fname:
             if rename:
                 info = info + 'new name: {} '.format(newname)
@@ -210,9 +265,13 @@ def geometryHelp():
     parser.print_help()
     print geometry_help
 
+def geometryHelp():
+    parser.print_help()
+    print format_help
+
 def checkGeometrySpec(geometry):
     rPerc = r"^[0-9]{1,2}\%$"               # regex to match percentage
-    rCart = r"^[0-9]{0,10}\:[0-9]{0,10}$"   # regex to match cartesians
+    rCart = r"^[0-9]{0,10}\x[0-9]{0,10}$"   # regex to match cartesians
     if re.match(rPerc,geometry):
         return 'P'
     if re.match(rCart,geometry):
@@ -231,7 +290,7 @@ def getparser(alist=None):
                         help="Recurse into sub-directories")
     parser.add_argument("-c", "--camera-names",action="store_true", dest="rename", default=False,
                         help="Rename pictures that have an embedded camera name to unique and meaningful names.")
-    parser.add_argument("-d", "--dry_run",action="store_true", dest="dry", default=False,
+    parser.add_argument("-D", "--dry_run",action="store_true", dest="dry", default=False,
                         help="Dry run: show what will be done without actually doing it. (sets verbose too.)")
     parser.add_argument('-f', '--format', action='store', dest='timeformat', default=defaultTimeFormat,
                         metavar='FORMAT',help=timeformatHelp)
@@ -241,14 +300,16 @@ def getparser(alist=None):
                         help="Automatically rotate images.")
     parser.add_argument('-z', '--resize', action='store', dest='resize', default=None, metavar='GEOMETRY',
                         help='Resize Images by: x%% or (width:height) if width or heigh is blank, Automatically calcualte based on the present value.')
-    parser.add_argument('--help-geometry', action="store_true", dest="geohelp", default=False,
-                        help='Show additional help on GEOMETRY and exit.')
     parser.add_argument('list', metavar='PATH', type=str, nargs='*',
                         help='File(s) or sub-directories to process')
     parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', default=False,
                         help='Be chatty about what is being done.')
     parser.add_argument('-V','--version', action='store_true', dest='dispversion', default=False,
                         help='Show version information and exit.')
+    parser.add_argument('--help-geometry', action="store_true", dest="geohelp", default=False,
+                        help='Show additional help on GEOMETRY and exit.')
+    parser.add_argument('--help-format', action="store_true", dest="geohelp", default=False,
+                        help='Show help on formatting names for automatic renaming')
     return parser
 
 
@@ -281,6 +342,8 @@ if __name__ == '__main__':
     rename = args.rename
     verbose = args.verbose 
     dry = args.dry
+    #!!!
+    dry = True
     if dry:
         verbose = True
 
